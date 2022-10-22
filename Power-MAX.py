8241bffa3172943ec3e23bc2ad018249e61b2cd6
Sun-F1owers <116019343+Sun-F1owers@users.noreply.github.com>
2022-10-22 12:21:43 +0600

    Update Power-MAX.py

--- Power-MAX.py
+++ Power-MAX.py
@@ -119,9 +119,9 @@ for xd in range(10000):
 
     c=random.randrange(1, 9)
 
-    d='Nokia'4567s
+    d='Nokia'
 
-    e=random.randrange(100, 9999)yyyyy
+    e=random.randrange(100, 9999)
 
     f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
 
